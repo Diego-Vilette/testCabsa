38f10a45f06cd6b48c05a1d98c43f746be472f65
Vilette Diego <vilette.diego@gmail.com>
2021-09-16 11:31:29 +0100

    new data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
 
-INSERT INTO animal(id,name,legs) VALUES (1, 'Rabbit', 4);
-INSERT INTO animal(id,name,legs) VALUES (2, 'Bear', 4);
-INSERT INTO animal(id,name,legs) VALUES (3, 'Deer', 4);
-INSERT INTO animal(id,name,legs) VALUES (4, 'Snake', 0);
-INSERT INTO animal(id,name,legs) VALUES (5, 'Coocodrile', 4);
-INSERT INTO animal(id,name,legs) VALUES (6, 'Chicken', 2);
-INSERT INTO animal(id,name,legs) VALUES (7, 'Spider', 8);
+INSERT INTO animal(id,name,legs) VALUES (1, 'rabbit', 4);
+INSERT INTO animal(id,name,legs) VALUES (2, 'bear', 4);
+INSERT INTO animal(id,name,legs) VALUES (3, 'deer', 4);
+INSERT INTO animal(id,name,legs) VALUES (4, 'snake', 0);
+INSERT INTO animal(id,name,legs) VALUES (5, 'coocodrile', 4);
+INSERT INTO animal(id,name,legs) VALUES (6, 'chicken', 2);
+INSERT INTO animal(id,name,legs) VALUES (7, 'spider', 8);
 
 
 INSERT INTO food(id, name) VALUES (1, 'carrot');
@@ -14,3 +14,14 @@ INSERT INTO food(id, name) VALUES (3, 'leafs');
 INSERT INTO food(id, name) VALUES (4, 'insects');
 INSERT INTO food(id, name) VALUES (5, 'vermin');
 INSERT INTO food(id, name) VALUES (6, 'birdseed');
+
+INSERT INTO animal_diet (eaten_by_id, diet_id) VALUES (1, 1);
+INSERT INTO animal_diet (eaten_by_id, diet_id) VALUES (2, 2);
+INSERT INTO animal_diet (eaten_by_id, diet_id) VALUES (2, 1);
+INSERT INTO animal_diet (eaten_by_id, diet_id) VALUES (3, 3);
+INSERT INTO animal_diet (eaten_by_id, diet_id) VALUES (4, 4);
+INSERT INTO animal_diet (eaten_by_id, diet_id) VALUES (4, 5);
+INSERT INTO animal_diet (eaten_by_id, diet_id) VALUES (5, 5);
+INSERT INTO animal_diet (eaten_by_id, diet_id) VALUES (6, 4);
+INSERT INTO animal_diet (eaten_by_id, diet_id) VALUES (6, 5);
+INSERT INTO animal_diet (eaten_by_id, diet_id) VALUES (7, 4);
